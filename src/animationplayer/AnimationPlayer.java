17814d0eee2661ae81176f82ba1020025989e166
Evan <etrutten@gmail.com>
2022-04-04 23:10:09 -0400

    shape data reading (circle only)

--- src/animationplayer/AnimationPlayer.java
+++ src/animationplayer/AnimationPlayer.java
@@ -15,56 +15,74 @@ import static javafx.application.Application.launch;
 
 public class AnimationPlayer extends Application {
 
+    int frames;
+    int speed;
+    int elements;
+    Circle circle = new Circle();
+
     void loadAnimationFromFile(String fileName) {
-        
+
         BufferedReader reader;
-        
+
         try {
-            
+
             reader = new BufferedReader(new FileReader(fileName));
             String line = reader.readLine();
-            
+
             while (line != null) {
+
+                if (line.contains("frames")) {
+
+                    frames = Integer.parseInt(line.replaceAll("[^0-9]", ""));
+                    line = reader.readLine();
+                    speed = Integer.parseInt(line.replaceAll("[^0-9]", ""));
+                    line = reader.readLine();
+                    elements = Integer.parseInt(line);
+
+                }
                 
-                System.out.println(line);
+                if (line.equals("Circle")) { circle = CircleUtils.create(reader, line); }
+                // check for "Rect"
+                // check for "Line"
+                
                 line = reader.readLine();
-                
+
             }
 
             reader.close();
-            
+
         } catch (IOException e) {
-        
+
         }
         
     }
-    
+
     @Override
     public void start(Stage primaryStage) {
-        
+
         loadAnimationFromFile("animation.txt");
-        
-        Circle circle = CircleUtils.create(100, 200, 200, 5, Color.RED, Color.DARKRED);
+
+        // Circle circle = CircleUtils.create(100, 200, 200, 5, Color.RED, Color.DARKRED);
         Rectangle rectangle = RectangleUtils.create(300, 100, 600, 200, 5, Color.BLUE, Color.DARKBLUE);
         Line line = LineUtils.create(200, 400, 800, 800, 5, Color.GREEN);
-        
+
         Pane pane = new Pane();
         pane.getChildren().add(circle);
         pane.getChildren().add(rectangle);
         pane.getChildren().add(line);
-        
+
         Scene scene = new Scene(pane, 1600, 900, Color.WHITESMOKE);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Animation Player");
-        
+
         primaryStage.show();
-        
+
     }
-    
+
     public static void main(String args[]) {
-        
+
         launch(args);
-        
+
     }
-    
+
 }
--- src/animationplayer/CircleUtils.java
+++ src/animationplayer/CircleUtils.java
@@ -1,18 +1,51 @@
 package animationplayer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class CircleUtils {
-    
-    static Circle create(int r, int x, int y, int border, Color color, Color borderColor) {
+
+    static Circle create(BufferedReader reader, String line) throws IOException {
+
+        Circle circle = new Circle();
+
+        line = reader.readLine();
+        circle.setRadius(Integer.parseInt(line.replaceAll("[^0-9]", "")));
+        line = reader.readLine();
+        circle.setCenterX(Integer.parseInt(line.replaceAll("[^0-9]", "")));
+        line = reader.readLine();
+        circle.setCenterY(Integer.parseInt(line.replaceAll("[^0-9]", "")));
         
-        Circle output = new Circle(x, y, r, color);
-        output.setStrokeWidth(border);
-        output.setStroke(borderColor);
-        
-        return output;
-        
+        circle = checkExtraFeatures(reader, line, circle);
+        return circle;
+
     }
     
+    static Circle checkExtraFeatures(BufferedReader reader, String line, Circle circle) throws IOException {
+        
+        line = reader.readLine();
+        
+        if (line.contains("border")) {
+            
+            circle.setStrokeWidth(Integer.parseInt(line.replaceAll("[^0-9]", "")));
+            checkExtraFeatures(reader, line, circle);
+            
+        } else if (line.contains("color")) {
+            
+            // add fill color here
+            checkExtraFeatures(reader, line, circle);
+            
+        } else if (line.contains("borderColor")) {
+            
+            // add border color here
+            checkExtraFeatures(reader, line, circle);
+            
+        }
+        
+        return circle;
+        
+    }
+
 }
